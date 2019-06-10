0b26a98e1cded372cd1d675d3c9c31bee2ad2d6d
Omid Ebrahimi <ebrahimi.omid69@gmail.com>
2019-06-10 15:23:13 +0430

    improves UI in NotFound screen

--- src/app/components/Main/screens/NotFound/index.js
+++ src/app/components/Main/screens/NotFound/index.js
@@ -1,15 +1,21 @@
 import React from 'react';
-import {Grid, Row} from '@material/react-layout-grid/dist/index';
+import {Grid, Row} from '@material/react-layout-grid';
 import CellCenter from "../../../../../components/CellCenter";
+import image from './image.svg';
 
 export default function NotFound() {
     return (
         <Grid>
             <Row>
                 <CellCenter desktopColumns={10} tabletColumns={8} phoneColumns={4}>
-                    <h2 className='color-error flex justify-center'>
+                    <h2 className='color-error center'>
                         صفحه‌ای که دنبال آن بودید پیدا نشد!
                     </h2>
+                </CellCenter>
+            </Row>
+            <Row>
+                <CellCenter desktopColumns={2} tabletColumns={4} phoneColumns={2}>
+                    <img src={image} alt='404 - Not Found'/>
                 </CellCenter>
             </Row>
         </Grid>
