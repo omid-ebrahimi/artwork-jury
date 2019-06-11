aa63810c73fbf9871ff56e2c4d5b1c9d019d6a93
Omid Ebrahimi <ebrahimi.omid69@gmail.com>
2019-06-11 20:40:20 +0430

    refactors Login screen

--- src/app/components/Main/screens/Login/component.js
+++ src/app/components/Main/screens/Login/component.js
@@ -1,23 +1,24 @@
 import React from 'react';
 import {Grid, Row} from '@material/react-layout-grid';
+import {Input} from "@material/react-text-field";
 import Button from '@material/react-button';
+import {Headline5} from '@material/react-typography';
 import CellCenter from "../../../../../components/CellCenter";
 import PersianTextField from "../../../../../components/PersianTextField";
-import PersianInput from "../../../../../components/PersianInput";
 import './component.css';
 
 function Login() {
     return (
         <Grid>
             <Row className='main-row'>
-                <CellCenter dir='rtl' desktopColumns={4} tabletColumns={6} phoneColumns={4} className='flex flex-column justify-center items-center'>
-                    <h2 className='col-10'>ورود به پنل داوری</h2>
-                    <hr className='col-10'/>
+                <CellCenter dir='rtl' desktopColumns={4} tabletColumns={6} phoneColumns={4}
+                            className='flex flex-column justify-center items-center'>
+                    <Headline5 className='col-10'>ورود به پنل داوری</Headline5>
                     <PersianTextField label='نام کاربری' className='col-10 mt2' outlined>
-                        <PersianInput/>
+                        <Input/>
                     </PersianTextField>
                     <PersianTextField label='رمز عبور' className='col-10 mt2' outlined>
-                        <PersianInput/>
+                        <Input/>
                     </PersianTextField>
                     <Button className='col-10 mt2' raised>
                         ورود
