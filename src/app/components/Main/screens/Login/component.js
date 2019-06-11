4b265c90116eea1988531d31a60a8753882d3169
Omid Ebrahimi <ebrahimi.omid69@gmail.com>
2019-06-11 14:14:01 +0430

    implements UI of Login screen

--- src/app/components/Main/screens/Login/component.js
+++ src/app/components/Main/screens/Login/component.js
@@ -1,10 +1,30 @@
 import React from 'react';
+import {Grid, Row} from '@material/react-layout-grid';
+import Button from '@material/react-button';
+import CellCenter from "../../../../../components/CellCenter";
+import PersianTextField from "../../../../../components/PersianTextField";
+import PersianInput from "../../../../../components/PersianInput";
+import './component.css';
 
 function Login() {
     return (
-        <section>
-            Login Page
-        </section>
+        <Grid>
+            <Row className='main-row'>
+                <CellCenter dir='rtl' desktopColumns={4} tabletColumns={6} phoneColumns={4} className='flex flex-column justify-center items-center'>
+                    <h2 className='col-10'>ورود به پنل داوری</h2>
+                    <hr className='col-10'/>
+                    <PersianTextField label='نام کاربری' className='col-10 mt2' outlined>
+                        <PersianInput/>
+                    </PersianTextField>
+                    <PersianTextField label='رمز عبور' className='col-10 mt2' outlined>
+                        <PersianInput/>
+                    </PersianTextField>
+                    <Button className='col-10 mt2' raised>
+                        ورود
+                    </Button>
+                </CellCenter>
+            </Row>
+        </Grid>
     )
 }
 
