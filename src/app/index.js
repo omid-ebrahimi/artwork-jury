78aa9c523222bfe14af1692f7d955cd9bda90d32
Omid Ebrahimi <ebrahimi.omid69@gmail.com>
2019-06-12 16:38:37 +0430

    wraps store provider around App

--- src/app/index.js
+++ src/app/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import {BrowserRouter} from "react-router-dom";
+import Provider from './store/provider'
 import Main from "./components/Main";
 
 function App() {
     return (
-        <BrowserRouter>
-            <Main/>
-        </BrowserRouter>
+        <Provider>
+            <BrowserRouter>
+                <Main/>
+            </BrowserRouter>
+        </Provider>
     );
 }
 
